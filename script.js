8c4575e7c661a9423bf9a5d87ef3bea2f856afef
sumon-webs <sumon01333581557@gmail.com>
2026-02-22 11:08:02 +0600

    all variable added

---
+++ script.js
@@ -0,0 +1,22 @@
+const btnAll = document.getElementById("btn-all")
+const btnInterview = document.getElementById("btn-interview")
+const btnRejected = document.getElementById("btn-rejected")
+
+const totalCount = document.getElementById("total-count");
+const interviewCount = document.getElementById("interview-count");
+const rejectedCount = document.getElementById("rejected-count");
+
+const totalJob = document.getElementById("total-job")
+
+const mainContainer = document.getElementById("main-container")
+
+const interViewBtn = document.getElementById("interview-btn")
+const rejectedViewBtn = document.getElementById("rejected-btn")
+
+const filterSection = document.getElementById("filter-section")
+
+const jobCount = document.getElementById("available-job")
+
+const deletes = document.getElementById("delete")
+
+const availableStatus = document.getElementById("available-section")
