61be8e7f29f209363fef56d791f86f0ba63f717f
sumon-webs <sumon01333581557@gmail.com>
2026-02-22 11:10:24 +0600

    Main container addEvenlistener added

--- script.js
+++ script.js
@@ -29,3 +29,71 @@ function count() {
     rejectedCount.innerText = rejectList.length
 }
 count()
+
+
+
+mainContainer.addEventListener("click", function (id) {
+
+    if (id.target.classList.contains("delete")) {
+        id.target.parentElement.parentElement.parentElement.remove()
+        count()
+    }
+    if (id.target.classList.contains("btn-success")) {
+        const parent = id.target.parentNode.parentNode;
+
+        const companyName = parent.querySelector(".company-name").innerText
+        const jobName = parent.querySelector(".job-name").innerText
+        const salary = parent.querySelector(".salary").innerText
+        const status = parent.querySelector(".apply").innerText
+        const about = parent.querySelector(".about").innerText
+
+        parent.querySelector(".apply").innerText = "Apply"
+
+        const jobInfo = {
+            companyName,
+            jobName,
+            salary,
+            status: "Apply   ",
+            about
+        }
+
+        const exist = interviewList.find(item => item.companyName === jobInfo.companyName)
+        if (!exist) {
+            interviewList.push(jobInfo)
+        }
+
+        rejectList = rejectList.filter(item => item.jobName != jobInfo.jobName)
+
+
+        count()
+
+    } else if (id.target.classList.contains("btn-error")) {
+        const parent = id.target.parentNode.parentNode;
+
+        const companyName = parent.querySelector(".company-name").innerText
+        const jobName = parent.querySelector(".job-name").innerText
+        const salary = parent.querySelector(".salary").innerText
+        const status = parent.querySelector(".apply").innerText
+        const about = parent.querySelector(".about").innerText
+
+        parent.querySelector(".apply").innerText = "Rejected"
+
+        const jobInfo = {
+            companyName,
+            jobName,
+            salary,
+            status: "Rejected",
+            about
+        }
+
+        const exist = rejectList.find(item => item.companyName === jobInfo.companyName)
+        if (!exist) {
+            rejectList.push(jobInfo)
+        }
+
+        interviewList = interviewList.filter(item => item.jobName != jobInfo.jobName)
+
+        count()
+
+    }
+})
